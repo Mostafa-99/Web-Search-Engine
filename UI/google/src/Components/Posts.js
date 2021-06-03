691d6b9213d114c04c42d2ecaa5dbcde258d044d
Ahmed Mohamed Yehia <61057091+AhmedMohamedYehia@users.noreply.github.com>
2021-06-04 00:51:09 +0200

    UI integ with backend missing sugg

--- UI/google/src/Components/Posts.js
+++ UI/google/src/Components/Posts.js
@@ -7,19 +7,24 @@ const Posts = ({ posts, loading }) => {
 
   return (
     <ul className='list-group mb-4'>
-      {posts.map((post,num) => (
-        <li key={post.id} className='list-group-item'>
-          <div class="card">
-            <h5 class="card-header">{num+1}</h5>
-            <div class="card-body">
-              <h5 class="card-title">{post.name}</h5>
-              <p class="card-text mt-3">With supporting text below as a natural lead-in to additional content.</p>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+      {
+      posts.length == 0?
+        ""
+      :
+        posts.map((post,num) => (
+          <li key={post.id} className='list-group-item'>
+            <div class="card">
+              <h5 class="card-header">{num+1}</h5>
+              <div class="card-body">
+                <h5 class="card-title">{post.url}</h5>
+                <p class="card-text mt-3">Your Searched word appeared {post.tf} times on this website.</p>
+                {/* <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
+                <a href={post.url} class="btn btn-primary">{post.url}</a>
+              </div>
             </div>
-          </div>
-        </li>
-      ))}
+          </li>
+        ))
+      }
     </ul>
   );
 };
