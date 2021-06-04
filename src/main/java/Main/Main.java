fcd06cd1aeafe35c5c14bf8e2eaed693debdec3d
ibrahim <ibrahimwael99@gmail.com>
2021-06-04 04:38:23 +0200

    Indexer Errors Solved.

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,12 +1,13 @@
 package Main;
 
 import Crawler.Crawler;
+import DBManager.DBManager;
 import Indexer.Indexer;
 
 public class Main {
     static final int RESTART = 0;
     static final int RESUME = 1;
-    static final int state = RESUME;
+    static final int state = RESTART;
     static final int numberOfThreads = 5;
     static final int crawlingSize = 5;
 
@@ -16,13 +17,10 @@ public class Main {
         //c0.crawlerMain();
         Thread mainCrawlerThread = new Thread(c0);
         Thread mainIndexerThread = new Thread(i0);
-
         mainCrawlerThread.setName("Thread 0");
         mainCrawlerThread.start();
-        
-        mainIndexerThread.start();
-        
         mainCrawlerThread.join();
+        mainIndexerThread.start();
         mainIndexerThread.join();
     }
 }
